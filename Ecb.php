df372feef3efeabc3945220d5d7d48f363d964fc
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-01-01 09:37:15 +0300

    no message

---
+++ Ecb.php
@@ -0,0 +1,31 @@
+<?php
+namespace Dfe\CurrencyConvert;
+use Magento\Directory\Model\Currency\Import\AbstractImport;
+class Ecb extends AbstractImport {
+	/**
+	 * 2016-01-01
+	 * @override
+	 * @see \Magento\Directory\Model\Currency\Import\AbstractImport::_convert()
+	 * @used-by \Magento\Directory\Model\Currency\Import\AbstractImport::fetchRates()
+	 * @param string $currencyFrom
+	 * @param string $currencyTo
+	 * @return float
+	 */
+	protected function _convert($currencyFrom, $currencyTo) {
+		return df_float(df_a($this->rates($currencyFrom), $currencyTo, 0.0));
+	}
+
+	/**
+	 * 2016-01-01
+	 * @param string $base
+	 * @return array(string => float)
+	 */
+	private function rates($base) {
+		if (!isset($this->{__METHOD__}[$base])) {
+			$this->{__METHOD__}[$base] = df_a(@df_json_decode(df_http_get(
+				'http://api.fixer.io/latest', ['base' => $base])
+			), 'rates', []);
+		}
+		return $this->{__METHOD__}[$base];
+	}
+}
