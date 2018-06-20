3835f8f01fe1ac4d92a643c60ecfbad8df6f9777
Dmitry Fedyuk <dfediuk@gmail.com>
2018-06-20 23:34:04 +0600

    2.0.0

--- Settings.php
+++ Settings.php
@@ -7,7 +7,9 @@ final class Settings extends \Df\Config\Settings {
 	 * 2018-06-20
 	 * @return string|null
 	 */
-	function accessKey() {return $this->p();}
+	function accessKey() {return $this->b('customAccessKey') ? $this->p() :
+		'f3ec8c540499e4bcd75de4ee09fcbed1'
+	;}
 
 	/**
 	 * 2018-06-20
