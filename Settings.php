0f96f9d5f8eab768aeb467c995346612ca89ed8a
Dmitry Fedyuk <dfediuk@gmail.com>
2022-10-26 15:40:28 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -7,16 +7,13 @@ final class Settings extends \Df\Config\Settings {
 	 * 2018-06-20
 	 * @return string|null
 	 */
-	function accessKey() {return $this->b('customAccessKey') ? $this->p() :
-		'f3ec8c540499e4bcd75de4ee09fcbed1'
-	;}
+	function accessKey() {return $this->b('customAccessKey') ? $this->p() : 'f3ec8c540499e4bcd75de4ee09fcbed1';}
 
 	/**
 	 * 2018-06-20
 	 * @override
 	 * @see \Df\Config\Settings::prefix()
 	 * @used-by \Df\Config\Settings::v()
-	 * @return string
 	 */
-	protected function prefix() {return 'df_api/fixer';}
+	protected function prefix():string {return 'df_api/fixer';}
 }
